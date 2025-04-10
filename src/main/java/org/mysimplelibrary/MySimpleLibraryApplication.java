af2f26dfc4c04b7f8127e3c976c22780903b9a6e
Maxim Nikolus <nikolusmaxim409@gmail.com>
2025-04-10 19:01:12 +0200

    Add

---
+++ src/main/java/org/mysimplelibrary/MySimpleLibraryApplication.java
@@ -0,0 +1,13 @@
+package org.mysimplelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySimpleLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySimpleLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mysimplelibrary/MySimpleLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mysimplelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySimpleLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
